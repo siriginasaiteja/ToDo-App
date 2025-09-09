834df88068c5beffb0336f515f51ff1bdc6a32cb
siriginasaiteja <saitejasirigina0000@gmail.com>
2025-09-09 15:02:10 +0530

    updated about page

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './about.css'
+import { FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -12,7 +13,8 @@ const About = () => {
             such as java and javascript, With good knowledge in web development skills like
             Reactjs,Nodejs,Express,Mongodb and Mongoose, I am a dedicated graduate from 
             KL University, After completting BTech. I am eager to contribute my technical skills 
-            and enthusiasm for problem-solving to real-world projects.</p>
+            and enthusiasm for problem-solving to real-world projects.My Linked In <br/>
+            <a href='https://www.linkedin.com/in/saiteja-sirigina-603189226/'><FaLinkedin /></a></p>
         </div>
     </div>
   )
