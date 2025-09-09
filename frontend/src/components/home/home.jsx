f8d91e76eb6447f9778932082563ef6e6c46d4b6
S VEERA VENKATA SAI TEJA <99489750+siriginasaiteja@users.noreply.github.com>
2025-09-09 13:24:42 +0530

    Update home.jsx

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import './home.css';
+import { useSelector } from "react-redux";
 
 const Home=()=>{
-    
+    const isLoggedIn=useSelector((state)=> state.isLoggedIn);
     const user = sessionStorage.getItem("username") || "Guest";
     return(
         <>
@@ -14,9 +15,17 @@ const Home=()=>{
                 <p className="text">Become focused, Organized, and calm with
                     <br/>todo app.The World's #1 task manager app.
                 </p>
+                {isLoggedIn ?
+                <>
                 <a href="todo">
                 <button className="homebtn">Make Todo List</button>
                 </a>
+                </>:<>
+                <a href="signup">
+                <button className="homebtn">Make Todo List</button>
+                </a>
+                </>
+            }
             </div>
         </div>
 
